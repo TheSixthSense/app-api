35340f14fafd3108e462f6354edfafdbfbb14e4e
Albert <39831075+Jyoungho@users.noreply.github.com>
2022-07-11 17:20:29 +0900

    test code 기본값 세팅 (#4)

---
+++ src/test/java/com/app/api/config/BaseMockTest.java
@@ -0,0 +1,8 @@
+package com.app.api.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BaseMockTest extends BaseTest{
+}
