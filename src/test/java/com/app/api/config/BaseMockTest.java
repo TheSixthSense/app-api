763796a1b5291d6316cab323056e1caaacd71d18
Albert <39831075+Jyoungho@users.noreply.github.com>
2022-07-17 14:14:10 +0900

    feat: enum validate check 로직 추가 및 테스트 코드 추가 (#7)

--- src/test/java/com/app/api/config/BaseMockTest.java
+++
@@ -1,8 +0,0 @@
-package com.app.api.config;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class BaseMockTest extends BaseTest{
-}
