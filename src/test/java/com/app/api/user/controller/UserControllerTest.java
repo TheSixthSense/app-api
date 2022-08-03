fbc8a1fb5cd86deb5ac4ff0103077fc59bd8ed92
Albert <youngh0910@gmail.com>
2022-08-03 23:54:37 +0900

    test: userController mvc test 추가

--- src/test/java/com/app/api/user/controller/UserControllerTest.java
+++ src/test/java/com/app/api/user/controller/UserControllerTest.java
@@ -1,7 +1,75 @@
 package com.app.api.user.controller;
 
 import com.app.api.config.BaseTest;
+import com.app.api.user.enums.Gender;
+import com.app.api.user.enums.VegannerStage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@DisplayName("User")
 class UserControllerTest extends BaseTest {
+    protected String appleId = "001805.7d48278a5f8d4c618263bef5a616f7dc.1512";
+    protected String clientSecret = "eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLndoaXRlcGFlay5zZXJ2aWNlcyIsImV4cCI6MTU5ODgwMDEyOCwiaWF0IjoxNTk4Nzk5NTI4LCJzdWIiOiIwMDAxNDguZjA2ZDgyMmNlMGIyNDgzYWFhOTdkMjczYjA5NzgzMjUuMTcxNyIsIm5vbmNlIjoiMjBCMjBELTBTOC0xSzgiLCJjX2hhc2giOiJ1aFFiV0gzQUFWdEc1OUw4eEpTMldRIiwiZW1haWwiOiJpNzlmaWl0OWIzQHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImlzX3ByaXZhdGVfZW1haWwiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTk4Nzk5NTI4LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.GQBCUHza0yttOfpQ-J5OvyZoGe5Zny8pI06sKVDIJaQY3bdiphllg1_pHMtPUp7FLv3ccthcmqmZn7NWVoIPkc9-_8squ_fp9F68XM-UsERKVzBvVR92TwQuKOPFr4lRn-2FlBzN4NegicMS-IV8Ad3AKTIRMIhvAXG4UgNxgPAuCpHwCwEAJijljfUfnRYO-_ywgTcF26szluBz9w0Y1nn_IIVCUzAwYiEMdLo53NoyJmWYFWu8pxmXRpunbMHl5nvFpf9nK-OGtMJrmZ4DlpTc2Gv64Zs2bwHDEvOyQ1WiRUB6_FWRH5FV10JSsccMlm6iOByOLYd03RRH2uYtFw";
+    protected String nickName = "비건첼린져_TestModule";
+    protected Gender gender = Gender.MALE;
+    protected String birthDay = "19920910";
+    protected VegannerStage vegannerStage = VegannerStage.BEGINNER;
 
+    @Test
+    @Transactional
+    @Order(1)
+    @DisplayName("회원가입")
+    public void createUser() throws Exception {
+        // given
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("appleId", this.appleId);
+        requestData.put("clientSecret", this.clientSecret);
+        requestData.put("nickName", this.nickName);
+        requestData.put("gender", this.gender);
+        requestData.put("birthDay", this.birthDay);
+        requestData.put("vegannerStage", this.vegannerStage);
+
+        String requestBody = objectMapper.writeValueAsString(requestData);
+
+        // when && then
+        mockMvc.perform(post("/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("meta.userMessage")
+                        .value(messageComponent.getMessage("success.user.create")));
+    }
+
+    @Test
+    @Transactional
+    @Order(2)
+    @DisplayName("닉네임 중복확인")
+    public void checkDuplicateNickName() throws Exception {
+        // given
+        String nickName = this.nickName;
+
+        // when && then
+        mockMvc.perform(get("/check/nick-name")
+                        .param("nickName", nickName)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("meta.userMessage")
+                        .value(messageComponent.getMessage("success.user.nickname.unique")));
+    }
 }
--- src/test/java/com/app/api/user/service/UserServiceTest.java
+++ src/test/java/com/app/api/user/service/UserServiceTest.java
@@ -8,11 +8,9 @@ import com.app.api.user.enums.Gender;
 import com.app.api.user.enums.UserRoleType;
 import com.app.api.user.enums.VegannerStage;
 import com.app.api.user.repository.UserRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -25,42 +23,54 @@ class UserServiceTest extends BaseTest {
     @Autowired
     private UserRepository userRepository;
 
+    protected String appleId = "OOOOOOOO";
+    protected String clientSecret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RNb2R1bGUxMjNAZ21haWwuY29tIiwic3ViIjoiMTIzNDU2Nzg5MCIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0.TzUYx4lm5r1vpft69lbr4jTlQF1tsarxKmqb7cJRYV8";
+    protected Gender gender = Gender.MALE;
+    protected String birthDay = "19920910";
+    protected UserRoleType userRoleType = UserRoleType.USER;
+    protected String nickName = "testNickName123";
+    protected VegannerStage vegannerStage = VegannerStage.BEGINNER;
+
     @Nested
     @Order(1)
+    @Transactional
     @DisplayName("회원가입")
     class SignUp {
+        protected UserRegDTO userRegDTO;
+        @BeforeEach
+        void beforeEach() {
+            this.userRegDTO = UserRegDTO.builder()
+                    .appleId(appleId)
+                    .clientSecret(clientSecret)
+                    .gender(gender)
+                    .birthDay(birthDay)
+                    .userRoleType(userRoleType)
+                    .nickName(nickName)
+                    .vegannerStage(vegannerStage)
+                    .build();
+        }
+
         @Test()
         @Order(1)
+        @Transactional
         @DisplayName("정상 회원가입")
         void signup() {
-            // given
-            UserRegDTO userRegDTO = new UserRegDTO();
-            userRegDTO.setAppleId("OOOOOOOO");
-            userRegDTO.setClientSecret("eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLndoaXRlcGFlay5zZXJ2aWNlcyIsImV4cCI6MTU5ODgwMDEyOCwiaWF0IjoxNTk4Nzk5NTI4LCJzdWIiOiIwMDAxNDguZjA2ZDgyMmNlMGIyNDgzYWFhOTdkMjczYjA5NzgzMjUuMTcxNyIsIm5vbmNlIjoiMjBCMjBELTBTOC0xSzgiLCJjX2hhc2giOiJ1aFFiV0gzQUFWdEc1OUw4eEpTMldRIiwiZW1haWwiOiJpNzlmaWl0OWIzQHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImlzX3ByaXZhdGVfZW1haWwiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTk4Nzk5NTI4LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.GQBCUHza0yttOfpQ-J5OvyZoGe5Zny8pI06sKVDIJaQY3bdiphllg1_pHMtPUp7FLv3ccthcmqmZn7NWVoIPkc9-_8squ_fp9F68XM-UsERKVzBvVR92TwQuKOPFr4lRn-2FlBzN4NegicMS-IV8Ad3AKTIRMIhvAXG4UgNxgPAuCpHwCwEAJijljfUfnRYO-_ywgTcF26szluBz9w0Y1nn_IIVCUzAwYiEMdLo53NoyJmWYFWu8pxmXRpunbMHl5nvFpf9nK-OGtMJrmZ4DlpTc2Gv64Zs2bwHDEvOyQ1WiRUB6_FWRH5FV10JSsccMlm6iOByOLYd03RRH2uYtFw");
-            userRegDTO.setGender(Gender.MALE);
-            userRegDTO.setBirthDay("19920910");
-            userRegDTO.setUserRoleType(UserRoleType.USER);
-            userRegDTO.setNickName("testNickName");
-            userRegDTO.setVegannerStage(VegannerStage.BEGINNER);
+            // given - userRegDTO
 
+            // when & then
             userService.signup(userRegDTO);
             assert true;
         }
 
         @Test()
         @Order(2)
+        @Transactional
         @DisplayName("client secret 값 이상")
         void error_when_client_secret_value_is_wrong() {
             // given
-            UserRegDTO userRegDTO = new UserRegDTO();
-            userRegDTO.setAppleId("OOOOOOOO");
-            userRegDTO.setClientSecret("sss");
-            userRegDTO.setGender(Gender.MALE);
-            userRegDTO.setBirthDay("19920910");
-            userRegDTO.setUserRoleType(UserRoleType.USER);
-            userRegDTO.setNickName("testNickName");
-            userRegDTO.setVegannerStage(VegannerStage.BEGINNER);
+            userRegDTO.setClientSecret("wrongToken");
 
+            // when & then
             assertThrows(BizException.class,
                     () -> userService.signup(userRegDTO),
                     messageComponent.getMessage("exception.wrong.value.client.secret"));
@@ -68,18 +78,13 @@ class UserServiceTest extends BaseTest {
 
         @Test()
         @Order(3)
+        @Transactional
         @DisplayName("어드민 권한 생성불가")
         void error_when_create_admin_role() {
             // given
-            UserRegDTO userRegDTO = new UserRegDTO();
-            userRegDTO.setAppleId("OOOOOOOO");
-            userRegDTO.setClientSecret("eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLndoaXRlcGFlay5zZXJ2aWNlcyIsImV4cCI6MTU5ODgwMDEyOCwiaWF0IjoxNTk4Nzk5NTI4LCJzdWIiOiIwMDAxNDguZjA2ZDgyMmNlMGIyNDgzYWFhOTdkMjczYjA5NzgzMjUuMTcxNyIsIm5vbmNlIjoiMjBCMjBELTBTOC0xSzgiLCJjX2hhc2giOiJ1aFFiV0gzQUFWdEc1OUw4eEpTMldRIiwiZW1haWwiOiJpNzlmaWl0OWIzQHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImlzX3ByaXZhdGVfZW1haWwiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTk4Nzk5NTI4LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.GQBCUHza0yttOfpQ-J5OvyZoGe5Zny8pI06sKVDIJaQY3bdiphllg1_pHMtPUp7FLv3ccthcmqmZn7NWVoIPkc9-_8squ_fp9F68XM-UsERKVzBvVR92TwQuKOPFr4lRn-2FlBzN4NegicMS-IV8Ad3AKTIRMIhvAXG4UgNxgPAuCpHwCwEAJijljfUfnRYO-_ywgTcF26szluBz9w0Y1nn_IIVCUzAwYiEMdLo53NoyJmWYFWu8pxmXRpunbMHl5nvFpf9nK-OGtMJrmZ4DlpTc2Gv64Zs2bwHDEvOyQ1WiRUB6_FWRH5FV10JSsccMlm6iOByOLYd03RRH2uYtFw");
-            userRegDTO.setGender(Gender.MALE);
-            userRegDTO.setBirthDay("19920910");
             userRegDTO.setUserRoleType(UserRoleType.ADMIN);
-            userRegDTO.setNickName("testNickName");
-            userRegDTO.setVegannerStage(VegannerStage.BEGINNER);
 
+            // when & then
             assertThrows(BizException.class,
                     () -> userService.signup(userRegDTO),
                     messageComponent.getMessage("exception.user.create.admin.role"));
@@ -87,21 +92,13 @@ class UserServiceTest extends BaseTest {
 
         @Test()
         @Order(4)
+        @Transactional
         @DisplayName("중복된 email로 아이디 생성 불가")
         void error_when_email_is_duplicated() {
             // given
-            UserRegDTO userRegDTO1 = new UserRegDTO();
-            userRegDTO1.setAppleId("OOOOOOOO");
-            userRegDTO1.setClientSecret("eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLndoaXRlcGFlay5zZXJ2aWNlcyIsImV4cCI6MTU5ODgwMDEyOCwiaWF0IjoxNTk4Nzk5NTI4LCJzdWIiOiIwMDAxNDguZjA2ZDgyMmNlMGIyNDgzYWFhOTdkMjczYjA5NzgzMjUuMTcxNyIsIm5vbmNlIjoiMjBCMjBELTBTOC0xSzgiLCJjX2hhc2giOiJ1aFFiV0gzQUFWdEc1OUw4eEpTMldRIiwiZW1haWwiOiJpNzlmaWl0OWIzQHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImlzX3ByaXZhdGVfZW1haWwiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTk4Nzk5NTI4LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.GQBCUHza0yttOfpQ-J5OvyZoGe5Zny8pI06sKVDIJaQY3bdiphllg1_pHMtPUp7FLv3ccthcmqmZn7NWVoIPkc9-_8squ_fp9F68XM-UsERKVzBvVR92TwQuKOPFr4lRn-2FlBzN4NegicMS-IV8Ad3AKTIRMIhvAXG4UgNxgPAuCpHwCwEAJijljfUfnRYO-_ywgTcF26szluBz9w0Y1nn_IIVCUzAwYiEMdLo53NoyJmWYFWu8pxmXRpunbMHl5nvFpf9nK-OGtMJrmZ4DlpTc2Gv64Zs2bwHDEvOyQ1WiRUB6_FWRH5FV10JSsccMlm6iOByOLYd03RRH2uYtFw");
-            userRegDTO1.setGender(Gender.MALE);
-            userRegDTO1.setBirthDay("19920910");
-            userRegDTO1.setUserRoleType(UserRoleType.USER);
-            userRegDTO1.setNickName("testNickName1");
-            userRegDTO1.setVegannerStage(VegannerStage.BEGINNER);
-
             UserRegDTO userRegDTO2 = new UserRegDTO();
             userRegDTO2.setAppleId("OOOOOOO1");
-            userRegDTO2.setClientSecret("eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLndoaXRlcGFlay5zZXJ2aWNlcyIsImV4cCI6MTU5ODgwMDEyOCwiaWF0IjoxNTk4Nzk5NTI4LCJzdWIiOiIwMDAxNDguZjA2ZDgyMmNlMGIyNDgzYWFhOTdkMjczYjA5NzgzMjUuMTcxNyIsIm5vbmNlIjoiMjBCMjBELTBTOC0xSzgiLCJjX2hhc2giOiJ1aFFiV0gzQUFWdEc1OUw4eEpTMldRIiwiZW1haWwiOiJpNzlmaWl0OWIzQHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImlzX3ByaXZhdGVfZW1haWwiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTk4Nzk5NTI4LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.GQBCUHza0yttOfpQ-J5OvyZoGe5Zny8pI06sKVDIJaQY3bdiphllg1_pHMtPUp7FLv3ccthcmqmZn7NWVoIPkc9-_8squ_fp9F68XM-UsERKVzBvVR92TwQuKOPFr4lRn-2FlBzN4NegicMS-IV8Ad3AKTIRMIhvAXG4UgNxgPAuCpHwCwEAJijljfUfnRYO-_ywgTcF26szluBz9w0Y1nn_IIVCUzAwYiEMdLo53NoyJmWYFWu8pxmXRpunbMHl5nvFpf9nK-OGtMJrmZ4DlpTc2Gv64Zs2bwHDEvOyQ1WiRUB6_FWRH5FV10JSsccMlm6iOByOLYd03RRH2uYtFw");
+            userRegDTO2.setClientSecret(clientSecret);
             userRegDTO2.setGender(Gender.MALE);
             userRegDTO2.setBirthDay("19920910");
             userRegDTO2.setUserRoleType(UserRoleType.USER);
@@ -110,56 +107,42 @@ class UserServiceTest extends BaseTest {
 
             assertThrows(BizException.class,
                     () -> {
-                        userService.signup(userRegDTO1);
+                        userService.signup(userRegDTO);
                         userService.signup(userRegDTO2);
                     },
                     messageComponent.getMessage("exception.user.already.exist"));
         }
 
         @Test()
-        @Order(4)
+        @Order(5)
+        @Transactional
         @DisplayName("중복된 nickName 사용 불가")
         void error_when_nick_name_is_duplicated() {
             // given
-            UserRegDTO userRegDTO1 = new UserRegDTO();
-            userRegDTO1.setAppleId("OOOOOOOO");
-            userRegDTO1.setClientSecret("eyJraWQiOiI4NkQ4OEtmIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLndoaXRlcGFlay5zZXJ2aWNlcyIsImV4cCI6MTU5ODgwMDEyOCwiaWF0IjoxNTk4Nzk5NTI4LCJzdWIiOiIwMDAxNDguZjA2ZDgyMmNlMGIyNDgzYWFhOTdkMjczYjA5NzgzMjUuMTcxNyIsIm5vbmNlIjoiMjBCMjBELTBTOC0xSzgiLCJjX2hhc2giOiJ1aFFiV0gzQUFWdEc1OUw4eEpTMldRIiwiZW1haWwiOiJpNzlmaWl0OWIzQHByaXZhdGVyZWxheS5hcHBsZWlkLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImlzX3ByaXZhdGVfZW1haWwiOiJ0cnVlIiwiYXV0aF90aW1lIjoxNTk4Nzk5NTI4LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9.GQBCUHza0yttOfpQ-J5OvyZoGe5Zny8pI06sKVDIJaQY3bdiphllg1_pHMtPUp7FLv3ccthcmqmZn7NWVoIPkc9-_8squ_fp9F68XM-UsERKVzBvVR92TwQuKOPFr4lRn-2FlBzN4NegicMS-IV8Ad3AKTIRMIhvAXG4UgNxgPAuCpHwCwEAJijljfUfnRYO-_ywgTcF26szluBz9w0Y1nn_IIVCUzAwYiEMdLo53NoyJmWYFWu8pxmXRpunbMHl5nvFpf9nK-OGtMJrmZ4DlpTc2Gv64Zs2bwHDEvOyQ1WiRUB6_FWRH5FV10JSsccMlm6iOByOLYd03RRH2uYtFw");
-            userRegDTO1.setGender(Gender.MALE);
-            userRegDTO1.setBirthDay("19920910");
-            userRegDTO1.setUserRoleType(UserRoleType.USER);
-            userRegDTO1.setNickName("testNickName");
-            userRegDTO1.setVegannerStage(VegannerStage.BEGINNER);
-
             UserRegDTO userRegDTO2 = new UserRegDTO();
-            userRegDTO2.setAppleId("OOOOOOO1");
+            userRegDTO2.setAppleId(appleId);
             userRegDTO2.setClientSecret("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJ0ZXN0QGdvb2dsZS5jb20iLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.ZQ7diVSCBsZpPc5X7Z70mt4yEUGSZm0ctdWlO3tpL30");
-            userRegDTO2.setGender(Gender.MALE);
-            userRegDTO2.setBirthDay("19920910");
-            userRegDTO2.setUserRoleType(UserRoleType.USER);
-            userRegDTO2.setNickName("testNickName");
+            userRegDTO2.setGender(gender);
+            userRegDTO2.setBirthDay(birthDay);
+            userRegDTO2.setUserRoleType(userRoleType);
+            userRegDTO2.setNickName(nickName);
             userRegDTO2.setVegannerStage(VegannerStage.BEGINNER);
 
             assertThrows(BizException.class,
                     () -> {
-                        userService.signup(userRegDTO1);
+                        userService.signup(userRegDTO);
                         userService.signup(userRegDTO2);
                     },
                     messageComponent.getMessage("exception.user.nickName.already.exist"));
         }
 
         @Test()
-        @Order(5)
+        @Order(6)
+        @Transactional
         @DisplayName("토큰값에서 이메일을 찾을 수 없습니다.")
         void error_when_client_secret_do_not_containe_email() {
             // given
-            UserRegDTO userRegDTO = new UserRegDTO();
-            userRegDTO.setAppleId("OOOOOOO1");
             userRegDTO.setClientSecret("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
-            userRegDTO.setGender(Gender.MALE);
-            userRegDTO.setBirthDay("19920910");
-            userRegDTO.setUserRoleType(UserRoleType.USER);
-            userRegDTO.setNickName("testNickName");
-            userRegDTO.setVegannerStage(VegannerStage.BEGINNER);
 
             assertThrows(BizException.class,
                     () -> userService.signup(userRegDTO),
@@ -168,28 +151,30 @@ class UserServiceTest extends BaseTest {
     }
 
     @Nested
+    @Transactional
     @Order(2)
     @DisplayName("닉네임 중복확인")
     class DuplicateNickName {
 
         @Test()
         @Order(1)
+        @Transactional
         @DisplayName("중복된 nickName 사용 불가")
         void error_when_nick_name_is_duplicated() {
+            // given
             userRepository.save(User.builder()
-                    .appleId("00000000")
+                    .appleId(appleId)
                     .email("test@gmail.com")
-                    .gender(Gender.MALE)
-                    .birthDay("19920910")
-                    .userRoleType(UserRoleType.USER)
-                    .nickName("testNickName1")
-                    .vegannerStage(VegannerStage.BEGINNER)
+                    .gender(gender)
+                    .birthDay(birthDay)
+                    .userRoleType(userRoleType)
+                    .nickName(nickName)
+                    .vegannerStage(vegannerStage)
                     .build());
 
-            String testNickName = "testNickName1";
-
+            // when & then
             assertThrows(BizException.class,
-                    () -> userService.checkDuplicateNickname(testNickName),
+                    () -> userService.checkDuplicateNickname(nickName),
                     messageComponent.getMessage("exception.user.nickName.already.exist"));
         }
     }
