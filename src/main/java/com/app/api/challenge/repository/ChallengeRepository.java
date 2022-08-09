45a6f03e855f1c6a513fa08ab35f97d9542c9768
Yumi Lee <eo8um@naver.com>
2022-08-09 14:45:47 +0900

    feat: 카테고리별 챌린지 목록 조회 추가

--- src/main/java/com/app/api/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/app/api/challenge/repository/ChallengeRepository.java
@@ -4,6 +4,10 @@ import com.app.api.challenge.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+
+    List<Challenge> findByCategoryId(Long categoryId);
 }
--- src/main/java/com/app/api/challenge/service/ChallengeService.java
+++ src/main/java/com/app/api/challenge/service/ChallengeService.java
@@ -1,12 +1,27 @@
 package com.app.api.challenge.service;
 
+import com.app.api.challenge.entity.Challenge;
 import com.app.api.challenge.repository.ChallengeRepository;
+import com.app.api.core.exception.BizException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChallengeService {
 
     private final ChallengeRepository challengeRepository;
+
+    public List<Challenge> getChallengeListByCategoryId(Long categoryId) {
+        List<Challenge> challengeList = challengeRepository.findByCategoryId(categoryId);
+
+        if (challengeList.isEmpty()) {
+            throw BizException.withUserMessageKey("exception.challenge.list.not.found").build();
+        }
+
+        return challengeList;
+
+    }
 }
