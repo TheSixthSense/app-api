c25ef6217116d55706f80a5ab788df85ae670b25
Yumi Lee <eo8um@naver.com>
2022-08-16 22:26:18 +0900

    feat: 유저 챌린지 참여 entity 추가

---
+++ src/main/java/com/app/api/challenge/entity/UserChallenge.java
@@ -0,0 +1,41 @@
+package com.app.api.challenge.entity;
+
+import com.app.api.challenge.enums.ChallengeStatus;
+import com.app.api.common.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_challenge")
+public class UserChallenge extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long challengeId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ChallengeStatus verificationStatus;
+
+    @Column
+    private LocalDateTime verificationDate;
+
+    @Column
+    private String verificationImage;
+
+}
