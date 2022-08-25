2c466a833cd3dc689e5a8749713bc88b604e7b85
Albert <39831075+Jyoungho@users.noreply.github.com>
2022-08-25 10:36:21 +0900

    feat: 회원정보 조회 및 수정 api 추가 (#42)

--- src/main/java/com/app/api/user/service/UserService.java
+++ src/main/java/com/app/api/user/service/UserService.java
@@ -3,6 +3,8 @@ package com.app.api.user.service;
 import com.app.api.core.exception.BizException;
 import com.app.api.user.dto.UserDTO;
 import com.app.api.user.dto.UserRegDTO;
+import com.app.api.user.dto.UserUpdateDTO;
+import com.app.api.user.dto.UserViewDTO;
 import com.app.api.user.entity.User;
 import com.app.api.user.entity.UserWithdraw;
 import com.app.api.user.enums.UserRoleType;
@@ -84,6 +86,26 @@ public class UserService {
         UserWithdraw userWithdraw = modelMapper.map(user, UserWithdraw.class);
         userWithdrawRepository.save(userWithdraw);
         userRepository.deleteById(userDTO.getId());
+    }
+
+    /**
+     * 회원정보 조회
+     */
+    public UserViewDTO convert(UserDTO userDTO) {
+        return modelMapper.map(userDTO, UserViewDTO.class);
+    }
+
+    /**
+     * 회원정보 수정
+     */
+    @Transactional
+    public UserViewDTO updateUserInfo(UserDTO userDTO, UserUpdateDTO userUpdateDTO) {
+        User user = userRepository.findById(userDTO.getId())
+                .orElseThrow(() -> BizException.withUserMessageKey("exception.user.not.found").build());
+
+        user.updateUserInfo(userUpdateDTO);
+        userRepository.save(user);
+        return modelMapper.map(user, UserViewDTO.class);
     }
 
     /**
