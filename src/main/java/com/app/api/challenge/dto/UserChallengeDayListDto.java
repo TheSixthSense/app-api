60ceef9bb2a3650a33c050e50789eaedd6b213ac
Albert <youngh0910@gmail.com>
2022-09-10 15:45:39 +0900

    fix: 유저의 챌린지 일별 조회에서 챌린지 인증 정보도 같이 불러 올 수 있도록 반영

--- src/main/java/com/app/api/challenge/dto/UserChallengeDayListDto.java
+++ src/main/java/com/app/api/challenge/dto/UserChallengeDayListDto.java
@@ -3,12 +3,10 @@ package com.app.api.challenge.dto;
 import com.app.api.challenge.enums.ChallengeStatus;
 import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
 import java.time.LocalDateTime;
 
 @Getter
@@ -16,33 +14,52 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class UserChallengeDayListDto {
 
-    @ApiModelProperty(value = "유저 챌린지 ID", example = "1", required = true)
+    @ApiModelProperty(value = "유저 챌린지 ID", example = "1")
     private long id;
 
-    @ApiModelProperty(value = "챌린지 ID", example = "2", required = true)
+    @ApiModelProperty(value = "유저 ID", example = "1")
+    private long userId;
+
+    @ApiModelProperty(value = "챌린지 ID", example = "2")
     private long challengeId;
 
-    @ApiModelProperty(value = "챌린지명", example = "우유 대신 두유 먹기", required = true)
+    @ApiModelProperty(value = "챌린지 날짜", example = "2022-08-30T00:00:00")
+    private LocalDateTime challengeDate;
+
+    @ApiModelProperty(value = "챌린지명", example = "우유 대신 두유 먹기")
     private String name;
 
     @ApiModelProperty(value = "챌린지 이모지", example = "\uD83C\uDF3E")
     private String emoji;
 
-    @ApiModelProperty(value = "챌린지 날짜", example = "2022-08-30T00:00:00", required = true)
-    private LocalDateTime challengeDate;
-
     @ApiModelProperty(value = "챌린지 인증 상태", example = "WAITING")
     private ChallengeStatus verificationStatus;
 
+    @ApiModelProperty(value = "챌린지 인증 날짜", example = "2022-08-30T00:00:00")
+    private LocalDateTime verificationDate;
+
+    @ApiModelProperty(value = "챌린지 인증 메모", example = "챌린지 인증 메모...ㅎㅎ")
+    private String verificationMemo;
+
+    @ApiModelProperty(value = "챌린지 인증 사진", example = "https://...")
+    private String verificationImage;
+
     @QueryProjection
     @Builder
-    public UserChallengeDayListDto(long id, long challengeId, String name, String emoji, LocalDateTime challengeDate, ChallengeStatus verificationStatus) {
+    public UserChallengeDayListDto(
+            long id, long userId, long challengeId, LocalDateTime challengeDate, String name, String emoji,
+            ChallengeStatus verificationStatus, LocalDateTime verificationDate, String verificationMemo,
+            String verificationImage) {
         this.id = id;
+        this.userId = userId;
         this.challengeId = challengeId;
+        this.challengeDate = challengeDate;
         this.name = name;
         this.emoji = emoji;
-        this.challengeDate = challengeDate;
         this.verificationStatus = verificationStatus;
+        this.verificationDate = verificationDate;
+        this.verificationMemo = verificationMemo;
+        this.verificationImage = verificationImage;
     }
 }
 
